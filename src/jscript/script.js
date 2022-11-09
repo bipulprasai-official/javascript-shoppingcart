79e25cf6e76959fa3bcea7b7f3f0d2f474aa8862
bipul prasai <bipulprasai2020@gmail.com>
2022-11-10 02:07:00 +1100

    updated cart function

--- src/jscript/script.js
+++ src/jscript/script.js
@@ -1,73 +1,127 @@
 let shop = document.getElementById("shop");
-console.log(shop);
+let basket = [];
 
-let shopItemData = [{
+let shopItemData = [
+  {
     id: "1234",
     name: "AIR JORDAN 1 RETRO HIGH TWIST W",
     price: "450",
-    description: "HYPESTEIN guarantees 100% authenticity of this or any other product found via our store, following the Swedish and EU laws on authentic goods.",
+    description:
+      "HYPESTEIN guarantees 100% authenticity of this or any other product found via our store, following the Swedish and EU laws on authentic goods.",
     quantity: 5,
-    img: "https://cdn.shopify.com/s/files/1/2200/1967/products/air-jordan-1-retro-high-twist-w-sneakers-nike_800x.jpg?v=1571712087"
-}, {
+    img: "https://cdn.shopify.com/s/files/1/2200/1967/products/air-jordan-1-retro-high-twist-w-sneakers-nike_800x.jpg?v=1571712087",
+  },
+  {
     id: "1235",
     name: "Nike Dunk High Retro",
     price: "550",
-    description: "Created for the hardwood but taken to the streets, the '80s b-ball icon returns with classic details and throwback hoops flair. Channeling vintage style back onto the streets, the padded, high-top collar of the Nike Dunk High lets you take your game anywhere—in comfort",
+    description:
+      "Created for the hardwood but taken to the streets, the '80s b-ball icon returns with classic details and throwback hoops flair. Channeling vintage style back onto the streets, the padded, high-top collar of the Nike Dunk High lets you take your game anywhere—in comfort",
     quantity: 10,
-    img: "https://images.footlocker.com/is/image/FLEU/244101498004_01?wid=2000&hei=2000&fmt=png-alpha"
-}, {
+    img: "https://images.footlocker.com/is/image/FLEU/244101498004_01?wid=2000&hei=2000&fmt=png-alpha",
+  },
+  {
     id: "1236",
     name: "Nike Dunk Hight Lakers",
     price: "680",
-    description: "With its classic basketball design, the Nike Dunk High Retro brings vintage '80s style back to the streets.The flawless leather upper sports a slight sheen and durable overlays reminiscent of '80s basketball. The padded, high-top collar provides a classic look while offering",
+    description:
+      "With its classic basketball design, the Nike Dunk High Retro brings vintage '80s style back to the streets.The flawless leather upper sports a slight sheen and durable overlays reminiscent of '80s basketball. The padded, high-top collar provides a classic look while offering",
     quantity: 5,
-    img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQcrWmq1xfDImNh3W2oIFS-xNXYTMkP7AwpD69Y4r6uedDvnrOFTvdpZoxyewSmvxYgAPjNmKDAhqya3A_xrOON8C73bN7DUrsa_F1igLEurBEBPmLRqy-y"
-}, {
+    img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQcrWmq1xfDImNh3W2oIFS-xNXYTMkP7AwpD69Y4r6uedDvnrOFTvdpZoxyewSmvxYgAPjNmKDAhqya3A_xrOON8C73bN7DUrsa_F1igLEurBEBPmLRqy-y",
+  },
+  {
     id: "1237",
     name: "adidas originals S.E.E.D. x Womens",
     price: "350",
-    description: "adidas originals S.E.E.D. x Womens WMNS Forum Mid 'Multi' Skate Shoes (SNKR/Women's/Multicolor) GV7673 (US 5½)",
+    description:
+      "adidas originals S.E.E.D. x Womens WMNS Forum Mid 'Multi' Skate Shoes (SNKR/Women's/Multicolor) GV7673 (US 5½)",
     quantity: 5,
-    img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRa04uJe3zWhqGEDDuiezlrQu14b7PeGSuipgxVQK_zEAoMbD_1RxYlgdH1uCkpjTpatQU5VKj3GzG_Kyl8xZJQZi4Cb_GO2yFxxj11TYz9ASU5pJ-q48nRVw"
-}];
+    img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRa04uJe3zWhqGEDDuiezlrQu14b7PeGSuipgxVQK_zEAoMbD_1RxYlgdH1uCkpjTpatQU5VKj3GzG_Kyl8xZJQZi4Cb_GO2yFxxj11TYz9ASU5pJ-q48nRVw",
+  },
+];
 
+// /////////////////
 // generating data from api to products list.
+// /////////////////
+
 let generateShop = () => {
-    return (shop.innerHTML = shopItemData.map((item) => {
-        return `<div class="item" key=${item.id}>
+  return (shop.innerHTML = shopItemData
+    .map((item) => {
+      let { id, name, price, description, quantity, img } = item;
+      return `<div class="item" key=${id}>
 <img width="215"
-    src=${item.img}
-    alt=${item.name}>
+    src=${img}
+    alt=${name}>
 <div class="itemdetail">
-    <h3 class="item-title">${item.name}</h3>
-    <p class="item-desc">${item.description}</p>
+    <h3 class="item-title">${name}</h3>
+    <p class="item-desc">${description}</p>
 <div class="quantity-price">
-<h2 class="item-price">$${item.price}</h2>
+<h2 class="item-price">$${price}</h2>
 <div class="item-buttons">
-<i onclick= "decreaseQuantity(${item.id})" class="bi bi-dash-lg"></i>
-    <div id=${item.id} class="quantity">${item.quantity}</div>
-    <i onclick="increaseQuantity(${item.id})" class="bi bi-plus-lg"></i>
+<i onclick= "decreaseQuantity(${id})" class="bi bi-dash-lg"></i>
+    <div id=${id} class="quantity">${quantity}</div>
+    <i onclick="increaseQuantity(${id})" class="bi bi-plus-lg"></i>
 </div>
 </div>
 </div>
-</div>`
-    }).join(""))
-}
+</div>`;
+    })
+    .join(""));
+};
 
 generateShop();
 
-// quantity function
+// /////////////////
+// quantity and add to cart function
+// /////////////////
 
 let increaseQuantity = (id) => {
-    let selectedItem = id;
-    console.log(selectedItem)
+  let selectedItem = id;
+  let search = basket.find((item) => item.id === selectedItem);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+
+//   console.log(basket);
+  updateQuantity(selectedItem);
 };
 
-basket.push({
-    id:selectedItem.id,
-    item:1
-})
+
+// /////////////////
+// removing quantity from cart
+///////////
 let decreaseQuantity = (id) => {
-    console.log(id)
+  let selectedItem = id;
+  let search = basket.find((item) => item.id === selectedItem);
+
+  if (search === 0) return;
+  else {
+    search.item -= 1;
+  }
+
+//   console.log(basket);
+  updateQuantity(selectedItem);
 };
-let updateQuantity = () => { };
+
+
+// /////////////////
+// update cart with quantity
+//// /////////////////
+let updateQuantity = (id) => {
+
+    let search = basket.find((item) => item.id === id);
+//   console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculate();
+};
+
+let calculate = () => {
+    let cartIcon = document.getElementById("cart-amount")
+cartIcon.innerHTML = basket.map(item => item.item).reduce((x,y) => x + y,0);
+}
